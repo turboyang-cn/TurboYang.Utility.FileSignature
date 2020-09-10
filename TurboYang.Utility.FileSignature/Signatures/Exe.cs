9baa0a5e00a2ce3270e556f91098fb60020dad23
Kimi Yang <89900101@qq.com>
2020-09-10 09:16:08 +0800

    Add file type (.exe)

---
+++ TurboYang.Utility.FileSignature/Signatures/Exe.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TurboYang.Utility.FileSignature.Matchers;
+
+namespace TurboYang.Utility.FileSignature.Signatures
+{
+    public class Exe : Signature
+    {
+        public Exe()
+            : base(".exe", "Windows/DOS Executable File", "application/x-msdownload", new GeneralMatcher(
+                new List<(Int32 Offset, Byte?[] MagicNumber)>()
+                {
+                    (0, new Byte?[] { 0x4D, 0x5A }),
+                })
+            )
+        {
+        }
+    }
+}
